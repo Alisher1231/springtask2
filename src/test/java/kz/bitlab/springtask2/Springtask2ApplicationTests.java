4da8db5abea95c25cb382fef156820ae9e87a2d5
Алишер <alisher.muhan@gmail.com>
2024-11-29 19:48:29 +0500

    Началный коммит в гит

---
+++ src/test/java/kz/bitlab/springtask2/Springtask2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.bitlab.springtask2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springtask2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
